9a0084ed13c90facc0886c5aa6572f7455a7491d
diazgaravitos@gmail.com <52507661+Playzr@users.noreply.github.com>
2022-11-17 13:43:57 -0500

    Update TimeOut Card Holder

--- R2000Demo/Form1.cs
+++ R2000Demo/Form1.cs
@@ -3296,7 +3296,7 @@ namespace R2000Demo
                             GuardarColorAsignacionTag(item.SubItems[1].Text, Convert.ToDateTime(item.SubItems[6].Text), item.SubItems[8].Text, Convert.ToInt32(item.SubItems[9].Text));
 
                         }
-
+                        //TODO: Si el tag es de tipo A se almacena la lectura en una lista
                         if (tagNuevo.Tipo == "A")
                         {
                             guardarLecturaTipoA = tagNuevo.Tipo;
@@ -3346,16 +3346,12 @@ namespace R2000Demo
 
                         }
                     }
+
+                    //TODO: Validacion Card Holder
                     if (guardarLecturaTipoC == "C" && guardarLecturaTipoA == "A")
                     {
-
-                        var intervalo = DateTime.Now - t1;
-                        MessageBox.Show("valor de intervalo: " + intervalo.ToString());
-
-                        //TODO: revisar tiempo para limpiar lista
-                        if (intervalo.TotalSeconds >= 4)
+                        if (DateTime.Now - OutLineTime > TimeSpan.FromSeconds(1))
                         {
-
                             foreach (AsignacionTag tag in listaTagsLeidos)
                             {
 
@@ -3374,10 +3370,7 @@ namespace R2000Demo
                                         itemToUpdate.SubItems[8].Text = tagNuevo.Color;
                                         itemToUpdate.BackColor = Color.FromName(tagNuevo.Color);
 
-                                        GuardarColor(item.SubItems[1].Text, item.SubItems[5].Text, Color.Red.Name.ToString());
                                         //variable que registra la hora en que pinto rojo t1
-
-                                        GuardarColorAsignacionTag(item.SubItems[1].Text, Convert.ToDateTime(item.SubItems[6].Text), item.SubItems[8].Text, Convert.ToInt32(item.SubItems[9].Text));
                                         ActivarAlarma(100);
 
                                     }
