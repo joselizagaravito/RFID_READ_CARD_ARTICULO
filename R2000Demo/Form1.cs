c69a157c8d9ac4af3001a06c61bda6d704a9f6df
diazgaravitos@gmail.com <52507661+Playzr@users.noreply.github.com>
2022-11-22 22:12:05 -0500

    Refactorizar limpiar lecturas

--- R2000Demo/Form1.cs
+++ R2000Demo/Form1.cs
@@ -281,14 +281,14 @@ namespace R2000Demo
             label_speed.Text = "";
             label_NumOfTags.Text = "";
             lB_times.Text = "";
-            StartTime = DateTime.Now;
+            //StartTime = DateTime.Now;
             LastTotalNumOfTags = 0;
             lb_totaltimes.Text = "";
             //guardarLecturaTipoC = "";
             //listaTagsAsignados.Clear();
 
-
         }
+
         private void button_inv_mul_Click(object sender, EventArgs e)
         {
             clicBtnMultiple();
@@ -2901,17 +2901,12 @@ namespace R2000Demo
         }
 
         DateTime OutLineTime;
-        //guardar un listview en un string 
-        
-        //ListViewItem itemCoincide = new ListViewItem();
 
         public void PageShow(int num)
         {
             //listView_Disp.Items.Clear();
             if (num <= 100)
             {
-                
-                //listaTagsLeidos.Clear();
                 for (long i = 0; i < num; i++)
                 {
                     ListViewItem item = new ListViewItem((listView_Disp.Items.Count + 1).ToString());
@@ -3001,8 +2996,6 @@ namespace R2000Demo
                             }
                         }
                     }
-                    
-                    //limpiar las variables
 
                     //TODO: Validacion Card Holder
                     if (guardarLecturaTipoC == "C" && guardarLecturaTipoA == "A")
@@ -3026,22 +3019,12 @@ namespace R2000Demo
                                         var itemToUpdate = listView_Disp.Items.Cast<ListViewItem>().Where(x => x.SubItems[1].Text == tag.Epc).FirstOrDefault();
                                         itemToUpdate.BackColor = Color.FromName(tagNuevo.Color);
                                         itemToUpdate.SubItems[8].Text = tagNuevo.Color;
-
-
-                                        //variable que registra la hora en que pinto rojo t1
                                         ActivarAlarma(100);
 
+
                                         //TODO: Detener Lectura
-                                        System.Threading.Thread.Sleep(1000);
-                                        button_inv_mul.PerformClick();
-                                        listView_Disp.Items.Clear();
-                                        guardarLecturaTipoC = "";
-                                        guardarLecturaTipoA = "";
-
-                                        button_inv_mul.PerformClick();
-
-
-
+                                        //clicBtnMultiple();
+                                        //System.Threading.Thread.Sleep(1000);
                                         //Fin
                                     }
                                     else
@@ -3054,8 +3037,6 @@ namespace R2000Demo
                                         var itemToUpdate = listView_Disp.Items.Cast<ListViewItem>().Where(x => x.SubItems[1].Text == tag.Epc).FirstOrDefault();
                                         itemToUpdate.SubItems[8].Text = tagNuevo.Color;
                                         itemToUpdate.BackColor = Color.FromName(tagNuevo.Color);
-                                        //OutLineTime = DateTime.Now;
-
                                     }
 
                                 }
@@ -3065,16 +3046,18 @@ namespace R2000Demo
 
                         listaTagsLeidos.Clear();
 
+                        if (estadoAlarmaActivada == true)
+                        {
+                            clicBtnMultiple();
+                            System.Threading.Thread.Sleep(2000);
+                        }
+
                         if (listaTagsAsignados.Count == listView_Disp.Items.Cast<ListViewItem>().Where(x => x.SubItems[8].Text == "Green").Count())
                         {
-                            button_inv_mul.PerformClick();
-                            listView_Disp.Items.Clear();
-                            guardarLecturaTipoC = "";
-                            guardarLecturaTipoA = "";
+                            detenerLectura_Clear();
                             System.Threading.Thread.Sleep(1000);
-                            button_inv_mul.PerformClick();
-
                         }
+
                     }
                 }
             }
@@ -3486,6 +3469,32 @@ namespace R2000Demo
         {
             mnuChangeVisiblity();
             btnChangeVisibility.Text = BasicParaSet.Visible ? "Menú Visible: SI" : "Menú Visible: NO";
+        }
+
+        private void StopClear_Click(object sender, EventArgs e)
+        {
+            detenerLectura_Clear();
+
+        }
+                //crear boton que detenga la lectura y limpie el listview y el label de lecturas 
+        private void detenerLectura_Clear()
+        {
+            m_Tags.Clear();
+            m_IndTag.Clear();
+            m_SortTag.Clear();
+            tagnum = 0;
+            lb_current.Text = "0";
+            lb_count.Text = "0";
+            listView_Disp.Items.Clear();
+            label_speed.Text = "";
+            label_NumOfTags.Text = "";
+            lB_times.Text = "";
+            LastTotalNumOfTags = 0;
+            lb_totaltimes.Text = "";
+
+            DetenerLecturas();
+            System.Threading.Thread.Sleep(1000);
+            ActivarLecturas();
         }
     }
 
